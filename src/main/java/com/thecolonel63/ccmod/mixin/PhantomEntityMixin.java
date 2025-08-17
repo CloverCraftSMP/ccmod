a9813baba4310b2a9da0dbab302a301c33141ff1
thecolonel63 <turkbuck@yahoo.com>
2025-08-17 18:55:31 -0500

    Make phantoms only attack players who haven't slept

---
+++ src/main/java/com/thecolonel63/ccmod/mixin/PhantomEntityMixin.java
@@ -0,0 +1,28 @@
+package com.thecolonel63.ccmod.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.TargetPredicate;
+import net.minecraft.entity.mob.PhantomEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.dimension.DimensionTypes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(targets = "net/minecraft/entity/mob/PhantomEntity$FindTargetGoal")
+public class PhantomEntityMixin {
+    @WrapOperation(method = "canStart", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/PhantomEntity;isTarget(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/entity/ai/TargetPredicate;)Z"))
+    private boolean checkInsomnia(PhantomEntity instance, LivingEntity livingEntity, TargetPredicate targetPredicate, Operation<Boolean> original) {
+        boolean canStart = original.call(instance, livingEntity, targetPredicate);
+
+        if (instance.getWorld().getDimensionEntry().matchesKey(DimensionTypes.OVERWORLD) && livingEntity instanceof ServerPlayerEntity serverPlayer) {
+            int sleepTicks = MathHelper.clamp(serverPlayer.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.TIME_SINCE_REST)), 1, Integer.MAX_VALUE);
+            return (sleepTicks >= 72000 && canStart);
+        }
+
+        return canStart;
+    }
+}
