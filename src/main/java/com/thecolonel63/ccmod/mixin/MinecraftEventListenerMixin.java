ccbf480c4694fd3c60a835e1fc7d507d2a918f24
thecolonel63 <turkbuck@yahoo.com>
2025-08-22 18:40:36 -0500

    Replace @Coerce with the actual type because I forgot how @Coerce works

--- src/main/java/com/thecolonel63/ccmod/mixin/MinecraftEventListenerMixin.java
+++ src/main/java/com/thecolonel63/ccmod/mixin/MinecraftEventListenerMixin.java
@@ -3,18 +3,18 @@ package com.thecolonel63.ccmod.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.xujiayao.discord_mc_chat.Config;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Coerce;
 
 @Pseudo
 @Mixin(targets = "com.xujiayao.discord_mc_chat.minecraft.MinecraftEventListener")
 public class MinecraftEventListenerMixin {
     @WrapOperation(method = "lambda$init$5", at = @At(value = "FIELD", target = "Lcom/xujiayao/discord_mc_chat/Config$Generic;announceDeathMessages:Z"), remap = false)
-    private static boolean addDeathMessageCheck(@Coerce Object instance, Operation<Boolean> original, @Local(argsOnly = true) ServerPlayerEntity player) {
+    private static boolean addDeathMessageCheck(Config.Generic instance, Operation<Boolean> original, @Local(argsOnly = true) ServerPlayerEntity player) {
         return original.call(instance) && player.getWorld().getGameRules().getBoolean(GameRules.SHOW_DEATH_MESSAGES);
     }
 }
