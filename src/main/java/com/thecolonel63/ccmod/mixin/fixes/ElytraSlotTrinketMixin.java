094da7bde8b5a9b0d6015ed0fb575ec71e6fe190
Syfe <22247810+ItsSyfe@users.noreply.github.com>
2025-09-23 22:19:25 +0100

    fix: elytra double slot break (#2)
    * fix: elytra double slot break
    
    * chore: change Redirect mixin to WrapOperation, also renamed method to give global reference

---
+++ src/main/java/com/thecolonel63/ccmod/mixin/fixes/ElytraSlotTrinketMixin.java
@@ -0,0 +1,32 @@
+package com.thecolonel63.ccmod.mixin.fixes;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.function.Consumer;
+
+@Mixin(targets = "com.illusivesoulworks.elytraslot.ElytraSlotFabricMod$1")
+public class ElytraSlotTrinketMixin {
+    @WrapOperation(
+            method = "tick(Lnet/minecraft/item/ItemStack;Ldev/emi/trinkets/api/SlotReference;Lnet/minecraft/entity/LivingEntity;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/ItemStack;damage(ILnet/minecraft/server/world/ServerWorld;Lnet/minecraft/server/network/ServerPlayerEntity;Ljava/util/function/Consumer;)V"
+            )
+    )
+    private void ccmod$skipDamageIfOneLeft(
+            ItemStack stack, int amount, ServerWorld serverWorld, ServerPlayerEntity serverPlayerEntity,
+            Consumer<Item> breakCallback, Operation<Void> original
+    ) {
+        int durabilityLeft = stack.getMaxDamage() - stack.getDamage();
+        if (durabilityLeft > 1) {
+            original.call(stack, amount, serverWorld, serverPlayerEntity, breakCallback);
+        }
+    }
+}
