8db8976bfd08937519e2e2111ee994188f4f614d
Syfe <22247810+ItsSyfe@users.noreply.github.com>
2025-09-23 22:17:43 +0100

    chore: change Redirect mixin to WrapOperation, also renamed method to give global reference

--- src/main/java/com/thecolonel63/ccmod/mixin/fixes/ElytraSlotTrinketMixin.java
+++ src/main/java/com/thecolonel63/ccmod/mixin/fixes/ElytraSlotTrinketMixin.java
@@ -1,30 +1,32 @@
 package com.thecolonel63.ccmod.mixin.fixes;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.function.Consumer;
 
 @Mixin(targets = "com.illusivesoulworks.elytraslot.ElytraSlotFabricMod$1")
 public class ElytraSlotTrinketMixin {
-    @Redirect(
+    @WrapOperation(
             method = "tick(Lnet/minecraft/item/ItemStack;Ldev/emi/trinkets/api/SlotReference;Lnet/minecraft/entity/LivingEntity;)V",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/item/ItemStack;damage(ILnet/minecraft/server/world/ServerWorld;Lnet/minecraft/server/network/ServerPlayerEntity;Ljava/util/function/Consumer;)V"
             )
     )
-    private void elytraslot$skipDamageIfOneLeft(
-            ItemStack stack, int amount, ServerWorld serverWorld, ServerPlayerEntity serverPlayerEntity, Consumer<Item> breakCallback
+    private void ccmod$skipDamageIfOneLeft(
+            ItemStack stack, int amount, ServerWorld serverWorld, ServerPlayerEntity serverPlayerEntity,
+            Consumer<Item> breakCallback, Operation<Void> original
     ) {
         int durabilityLeft = stack.getMaxDamage() - stack.getDamage();
         if (durabilityLeft > 1) {
-            stack.damage(amount, serverWorld, serverPlayerEntity, breakCallback);
+            original.call(stack, amount, serverWorld, serverPlayerEntity, breakCallback);
         }
     }
 }
